49a9ce9a784fcffd92c78da743fdaede1711f779
pmmaster12 <120505111+pmmaster12@users.noreply.github.com>
2024-04-04 01:03:04 +0530

    Add files via upload

---
+++ Health ChatBot/Health ChatBot/long_responses.py
@@ -0,0 +1,8 @@
+import random
+def unknown():
+    response = ["Could you please say it again ? ",
+                "...",
+                "what?",
+                "What does that mean?"][
+        random.randrange(4)]
+    return response
